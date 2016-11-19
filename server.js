c7a569f5bb3570ab546ce78a9985bab0c185151c
nisargkolhe <nisargkolhe@gmail.com>
2016-11-19 16:51:28 -0500

    added upload

--- server.js
+++ server.js
@@ -10,11 +10,62 @@ var path = require('path');
 var axios = require('axios');
 var http = require('http');
 var Clarifai = require('clarifai');
+var multer = require('multer');
+var fs = require('fs');
+
 var app     = express();
 
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+var storage = multer.diskStorage({
+	destination: function(req, file, callback) {
+		callback(null, './uploads');
+	},
+	filename: function(req, file, callback) {
+		callback(null, file.fieldname + '-' + Date.now());
+	}
+});
+
+function decodeBase64Image(dataString) {
+	var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+	var response = {};
+	if (matches.length !== 3) {
+		return new Error('Invalid input string');
+	}
+	response.type = matches[1];
+	response.data = new Buffer(matches[2], 'base64');
+	return response;
+}
+
+var upload = multer(); 
+
+// for parsing multipart/form-data
+app.post('/testFormData', upload.array(), function(req, res) {
+	var base64Data = decodeBase64Image(req.body.testdot);
+	//console.log('writing file...', base64Data);
+	var filename = Date.now() + ".jpg";
+	fs.writeFile(__dirname + "/uploads/" + filename, base64Data.data, function(err) {
+		if (err) {
+			console.log(err);
+			res.send({
+				"success": false,
+				"error": err
+			});
+		}
+		fs.readFile(__dirname + "/uploads/" + filename, function(err, data) {
+			if (err) {
+				res.send({
+					"success": false,
+					"error": err
+				});
+			}
+			console.log('reading file...', filename);
+			res.send({"success":true, "filename":filename});
+		});
+	});
+});
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/');
